c97eb54d95bda34c82ca431700190ec490e59efc
Nitin10cd <nsaxenacse@gmail.com>
2025-08-21 21:42:11 +0530

    changes

--- Heap/Find_median_in_the_stream.cpp
+++ Heap/Find_median_in_the_stream.cpp
@@ -1,9 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
   public:
     vector<double> getMedian(vector<int> &arr) {
-        // code here
+        vector<double> result;
+        priority_queue<int> left; // max heap
+        priority_queue<int, vector<int>, greater<int>> right; // min heap
         
+        for (int num : arr) {
+       
+            if (left.empty() || num <= left.top()) {
+                left.push(num);
+            } else {
+                right.push(num);
+            }
+            if ((int)left.size() > (int)right.size() + 1) {
+                right.push(left.top());
+                left.pop();
+            } else if ((int)right.size() > (int)left.size()) {
+                left.push(right.top());
+                right.pop();
+            }
+
+         
+            if (left.size() == right.size()) {
+                double median = (left.top() + right.top()) / 2.0;
+                result.push_back(median);
+            } else {
+                result.push_back(left.top());
+            }
+        }
+        return result;
     }
 };
