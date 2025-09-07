97d660eb33a82804a0ce6ab878eb22b9399f746b
Nitin10cd <nsaxenacse@gmail.com>
2025-09-07 15:19:03 +0530

    dp 2

---
+++ dp/maximum_sum_non_adjucent.dp.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    // memoisation dp
+    int maximum_sum_non_adjacent(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2  = 0;
+
+        for (int i = 1; i < n; i++) {
+            int take = nums[i];
+            if (i > 1) take += prev2;
+            int notTake = 0 + prev;
+
+            int curi = max(take, notTake);
+            prev2 = prev;
+            prev = curi; 
+        }
+        return prev;
+    }
+};
