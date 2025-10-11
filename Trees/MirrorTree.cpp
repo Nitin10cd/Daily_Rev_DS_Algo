dfa025c90c2789e608f08c498ae3425643739271
Nitin10cd <nsaxenacse@gmail.com>
2025-10-11 19:01:03 +0530

    adding in the repo

---
+++ Trees/MirrorTree.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+class Solution {
+  public:
+    void mirror(Node* root) {
+        // code here
+        if (!root) return;
+        Node* temp = root -> right;
+        root -> right = root -> left;
+        root -> left = temp;
+
+        // mirroring 
+        mirror(root -> left);
+        mirror(root -> right);
+    }
+};
